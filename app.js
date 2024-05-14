98177cf57dd7dfa55f313640e3c26cf90c10aee9
BluWindows <ginoMKjwfjh@gmai.com>
2024-05-14 19:42:29 +0200

    soa and secured all functions

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const mongoose = require('mongoose');
 
 //COSTUM
-const Ricetta = require('./public/ricettaCard');
+
 //PRENOTATORE
 const AppointmentPicker = require('appointment-picker');
 let pass = password.pass
-console.log(pass)
+
 // MongoDB connection string  DB STARTS
 const uri = `mongodb+srv://emilianopizzuti95:${pass}@chefsitedb.tcsakoi.mongodb.net/?retryWrites=true&w=majority&appName=chefSiteDB`;
  mongoose.connect(uri)
@@ -89,18 +89,20 @@ const Review = mongoose.model('Review', reviewSchema);
 //DB ENDS//
 
 //cards automation logic
+const Ricetta = require('./public/ricettaCard');
+const Evento = require('./public/eventoCard'); 
 const imageBasePath = '/recipes_photos'
 const imageDirectory = path.join(__dirname, 'img', 'recipes_photos');
 const allFiles = fs.readdirSync(imageDirectory);
+
+
 const imageFiles = allFiles.filter((file) => {
   const ext = path.extname(file).toLowerCase();
   return ['.jpeg', '.jpg', '.png', '.gif'].includes(ext);
 }); 
 
 const relativeImagePaths = imageFiles.map((file) => path.join('/', imageBasePath, file)); // Relative path starting from root of the project
-
-
-
+//recipes logic
 const recipes = relativeImagePaths.map((imagePath, index) => {
   return new Ricetta('', 'A sample description', imagePath);
 });
@@ -108,6 +110,27 @@ recipes.forEach((recipe, index) => {
   const imageName = path.basename(imageFiles[index], path.extname(imageFiles[index])); // Get the base name without extension
   recipe.name = imageName; // Assign the image name to the `Ricetta` instance
 });
+//events logic
+const eventi = [
+  new Evento(
+    'India Retreat', 
+    'Yoga e Cucina Ayurvedica: Pratica di yoga e corso di cucina ayurvedica per bilanciare i dosha e migliorare il benessere attraverso piatti equilibrati.', 
+    '/path/to/image1.jpg', 
+    '2024-05-15',
+    ),
+  new Evento(
+    'Portugal Retreat', 
+    'Yoga e Cucina Ayurvedica: Pratica di yoga e corso di cucina ayurvedica per bilanciare i dosha e migliorare il benessere attraverso piatti equilibrati.', 
+    './img/utilities/propic.jpeg', 
+    '2024-06-20',
+    ),
+  new Evento(
+    'Ibiza Retreat', 
+    'Yoga e Cucina Ayurvedica: Pratica di yoga e corso di cucina ayurvedica per bilanciare i dosha e migliorare il benessere attraverso piatti equilibrati.', 
+    '/path/to/image3.jpg', 
+    '2024-07-10',
+    )
+];
 
 
 // Middleware for parsing POST requests
@@ -120,10 +143,15 @@ server.set('views', path.join(__dirname, 'views'));
 server.use(express.static(path.join(__dirname, 'img')));
 
 // Routing
-server.get('/', (req, res) => {
-    res.render('index');
+server.get('/',async (req, res) => {
+  try {
+      const reviews = await Review.find();  // Fetch all reviews
+      res.render('index', { reviews });   // Pass reviews to EJS template
+      return reviews
+  } catch (error) {
+      res.status(500).send(error);
+  }
 });
-
 server.get('/about', async (req, res) => {
     try {
         const reviews = await Review.find(); // Fetch all reviews
@@ -150,20 +178,24 @@ server.get('/failure', (req, res) => {
 server.get('/review', (req, res) => {
   res.render('review',);
 });
-server.get('/reservations', async (req, res) => {
+
+
+server.get('/personal', async (req, res) => {
     try {
         const reservations = await Reservation.find();
         console.log(reservations); // This will log the output to your console
+        reservations.sort(function(a, b) {
+          return new Date(a.date) - new Date(b.date);
+        });
         res.render('reservations', { reservations });
     } catch (error) {
         res.status(500).send("Error retrieving reservations: " + error.message);
     }
 });
-
+//get events pages
 server.get('/events', (req, res) => {
-  res.render('events',);
+  res.render('events',{ eventi: eventi });
 });
-
 //Endpoint to handle reservations
 server.post('/book-appointment', async (req, res) => {
   try {
@@ -176,8 +208,6 @@ server.post('/book-appointment', async (req, res) => {
     res.status(500).redirect('/failure')
   }
 });
-
-
 server.post('/submit-review', async (req, res) => {
     try {
         const { name, rating, comments } = req.body;
@@ -185,7 +215,8 @@ server.post('/submit-review', async (req, res) => {
             reviewer: name,
             rating: rating,
             comments: comments,
-            date: new Date()
+            date: new Date(),
+            chefName: 'Doina'
         });
         await newReview.save();
         res.redirect('/about'); // Redirect to the reviews page or wherever appropriate
@@ -202,8 +233,6 @@ server.post('/submit-review', async (req, res) => {
         res.status(500).send(error);
     }
 });
-
-
 // 404 Error handling
 server.use((req, res) => {
     res.status(404).render('404');
---
+++ public/eventoCard.js
@@ -0,0 +1,10 @@
+class Evento {
+    constructor(name,desc,path,date ){
+        this.name = name;
+        this.desc = desc;
+        this.path = path;
+        this.date = date;
+    }
+}
+
+module.exports = Evento;
